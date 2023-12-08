f6b4275b9366d93e52eaa2d9b31bc52ce4773625
unknown <ezechelanre@gmail.com>
2023-12-08 10:45:04 +0100

    working on the da

--- src/pages/Dashboards/userDashboard/UserDashboard.jsx
+++ src/pages/Dashboards/userDashboard/UserDashboard.jsx
@@ -38,8 +38,7 @@ const UserDashboard = () => {
             <div className={
                 `dark:bg-main-bg bg-main-bg-min-h-screen w-full
                 ${activeMenu ? 
-                ' md:ml-72'
-                :  'flex-2'}`
+                ' md:ml-72' :  'flex-2'}`
             }>
                 <div className="fixed md:static
                     bg-main-bg dark:bg-main-dark-bg navbar w-full
@@ -50,10 +49,29 @@ const UserDashboard = () => {
                 <div>
                     <Routes>
                         {/* Dashboard */}
-                        <Route 
-                            path='/'
-                            element= 'Properties'
-                        />
+                        <Route path='/' element= 'Properties' />
+                        <Route path='/properties' element= 'Properties'/>
+
+                        {/* Pages */}
+                        <Route path='/orders' element='Orders' />
+                        <Route path='/agents' element='Agents' />
+                        <Route path='/customers' element='Customers' />
+
+                        {/* Apps */}
+                        <Route path='/kanban' element='Kanban' />
+                        <Route path='/editor' element='Editor' />
+                        <Route path='/calendar' element='Calendar' />
+                        <Route path='/color-picker' element='ColorPicker' />
+
+                        {/* charts */}
+                        <Route path='/line' element='Line' />
+                        <Route path='/area' element='Area' />
+                        <Route path='/bar' element='Bar' />
+                        <Route path='/pie' element='Pie' />
+                        <Route path='/financial' element='Financial' />
+                        <Route path='/color-mapping' element='ColorMapping' />
+                        <Route path='/pyramid' element='Pyramid' />
+                        <Route path='/stacked' element='Stacked' />
                     </Routes>
                 </div>
             </div>
