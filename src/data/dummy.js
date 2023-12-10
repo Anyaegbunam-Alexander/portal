e09a815cd5457bbc7e5d90cb826b11022bb8f38c
unknown <ezechelanre@gmail.com>
2023-12-10 19:15:19 +0100

    working on the dashboard's sidebar

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -474,7 +474,7 @@ export const links = [
     title: 'Dashboard',
     links: [
       {
-        name: 'ecommerce',
+        name: 'properties',
         icon: <FiShoppingBag />,
       },
     ],
