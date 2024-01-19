9cc5a06185e6e846f48b001fbe6388653660fd7f
LanreSam <ezechelanre@gmail.com>
2024-01-19 18:04:54 +0100

    working on the feature

--- src/pages/Dashboards/agencyDashboard/Properties.jsx
+++ src/pages/Dashboards/agencyDashboard/Properties.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { IoIosMore } from 'react-icons/io';
 
 
@@ -8,10 +8,58 @@ import house1 from '../../../data/company_x-1.jpg';
 
 const Properties = () => {
   const { currentColor } = useStateContext();
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    // Fetch properties from your API here
+    const fetchProperties = async () => {
+      try {
+        const response = await fetch('https://realestate.api.sites.name.ng/properties/');
+        const data = await response.json();
+        setProperties(data); // Assuming your API response is an array of properties
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
 
   return (
     <div className="mt-16">
-      <div className='flex flex-wrap justify-center w-100 bg-black'>
+      <div className='flex flex-wrap justify-center w-100'>
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Listing</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50"
+              src={house1}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
+              <p className="text-gray-400 ">Agency 1</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="View Property"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
           <div className="flex justify-between">
             <p className="text-xl font-semibold">Listing</p>
