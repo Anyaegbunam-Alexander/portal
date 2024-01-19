483025734ee2bd70a76edf41e4571d0ef4ef8156
LanreSam <ezechelanre@gmail.com>
2024-01-19 13:55:08 +0100

    Testing out the feature

--- src/pages/Dashboards/agencyDashboard/Properties.jsx
+++ src/pages/Dashboards/agencyDashboard/Properties.jsx
@@ -11,7 +11,7 @@ const Properties = () => {
 
   return (
     <div className="mt-16">
-      <div className='flex flex-wrap justify-center'>
+      <div className='flex flex-wrap justify-center w-100 bg-black'>
         <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
           <div className="flex justify-between">
             <p className="text-xl font-semibold">Listing</p>
@@ -21,12 +21,140 @@ const Properties = () => {
           </div>
           <div className="mt-10">
             <img
-              className="md:w-96 h-50 "
+              className="md:w-96 h-50"
               src={house1}
               alt=""
             />
             <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Dupluex</p>
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
+              <p className="text-gray-400 ">Agency 1</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="View Property"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Listing</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50"
+              src={house1}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
+              <p className="text-gray-400 ">Agency 1</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="View Property"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Listing</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50"
+              src={house1}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
+              <p className="text-gray-400 ">Agency 1</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="View Property"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Listing</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50"
+              src={house1}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
+              <p className="text-gray-400 ">Agency 1</p>
+              <p className="mt-8 text-sm text-gray-400">
+                This will be the small description for the news you have shown
+                here. There could be some great info.
+              </p>
+              <div className="mt-3">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="View Property"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Listing</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+          <div className="mt-10">
+            <img
+              className="md:w-96 h-50"
+              src={house1}
+              alt=""
+            />
+            <div className="mt-8">
+              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
               <p className="text-gray-400 ">Agency 1</p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
