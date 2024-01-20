ab78242fdc06d897bff4da79547648609bd6ab7a
LanreSam <ezechelanre@gmail.com>
2024-01-20 11:43:41 +0100

    Worked on the show properties feature

--- src/pages/Dashboards/agencyDashboard/Properties.jsx
+++ src/pages/Dashboards/agencyDashboard/Properties.jsx
@@ -9,14 +9,27 @@ import house1 from '../../../data/company_x-1.jpg';
 const Properties = () => {
   const { currentColor } = useStateContext();
   const [properties, setProperties] = useState([]);
-
+  
+  
   useEffect(() => {
+    const accessToken = localStorage.getItem('token');
     // Fetch properties from your API here
     const fetchProperties = async () => {
       try {
-        const response = await fetch('https://realestate.api.sites.name.ng/properties/');
+        const response = await fetch('https://realestate.api.sites.name.ng/properties/', {
+          method: 'GET',
+          headers: {
+            //"Referer": "https://realestate.api.sites.name.ng/",
+            'Authorization': `Bearer ${accessToken}`,
+          }
+        });
+
         const data = await response.json();
-        setProperties(data); // Assuming your API response is an array of properties
+
+        console.log('API response:', data); // Log the response
+        
+        // Assuming 'results' is the key containing the properties array
+        setProperties(data.results);
       } catch (error) {
         console.error('Error fetching properties:', error);
       }
@@ -25,200 +38,47 @@ const Properties = () => {
     fetchProperties();
   }, []);
 
+
   return (
     <div className="mt-16">
       <div className='flex flex-wrap justify-center w-100'>
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
+        {properties && properties.length > 0 ? (
+          properties.map(property => (
+            <div key={property.id} className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+              <div className="flex justify-between">
+                <p className="text-xl font-semibold">Listing</p>
+                <button type="button" className="text-xl font-semibold text-gray-500">
+                  <IoIosMore />
+                </button>
+              </div>
+              <div className="mt-10">
+                <img
+                  className="md:w-96 h-50"
+                  src={house1}
+                  alt=""
                 />
+                <div className="mt-8">
+                  <p className="font-semibold text-lg"> {property.type}</p>
+                  <p className="text-gray-400 ">Agency 1</p>
+                  <p className="mt-8 text-sm text-gray-400">
+                    This will be the small description for the news you have shown
+                    here. There could be some great info.
+                  </p>
+                  <div className="mt-3">
+                    <Button
+                      color="white"
+                      bgColor={currentColor}
+                      text="View Property"
+                      borderRadius="10px"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Listing</p>
-            <button type="button" className="text-xl font-semibold text-gray-500">
-              <IoIosMore />
-            </button>
-          </div>
-          <div className="mt-10">
-            <img
-              className="md:w-96 h-50"
-              src={house1}
-              alt=""
-            />
-            <div className="mt-8">
-              <p className="font-semibold text-lg"> Semi-detached Duplex</p>
-              <p className="text-gray-400 ">Agency 1</p>
-              <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
-              </p>
-              <div className="mt-3">
-                <Button
-                  color="white"
-                  bgColor={currentColor}
-                  text="View Property"
-                  borderRadius="10px"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+          )) 
+        ) : (
+            <p>No properties available.</p> 
+        )}
       </div>
     </div>
   )
