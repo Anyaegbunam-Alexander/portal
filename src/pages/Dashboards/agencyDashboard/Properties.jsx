2adead4f3d4a984e04388aa1f0f16542ac0d473a
LanreSam <ezechelanre@gmail.com>
2024-01-20 13:17:56 +0100

    finished the no property design

--- src/pages/Dashboards/agencyDashboard/Properties.jsx
+++ src/pages/Dashboards/agencyDashboard/Properties.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IoIosMore } from 'react-icons/io';
 
 
@@ -10,6 +11,12 @@ const Properties = () => {
   const { currentColor } = useStateContext();
   const [properties, setProperties] = useState([]);
   
+  const navigate = useNavigate();
+  const role = localStorage.getItem('role');
+
+  const handleClick = () => {
+    return navigate(`/${role}/add-property`);
+  }
   
   useEffect(() => {
     const accessToken = localStorage.getItem('token');
@@ -19,16 +26,13 @@ const Properties = () => {
         const response = await fetch('https://realestate.api.sites.name.ng/properties/', {
           method: 'GET',
           headers: {
-            //"Referer": "https://realestate.api.sites.name.ng/",
             'Authorization': `Bearer ${accessToken}`,
           }
         });
 
         const data = await response.json();
-
         console.log('API response:', data); // Log the response
         
-        // Assuming 'results' is the key containing the properties array
         setProperties(data.results);
       } catch (error) {
         console.error('Error fetching properties:', error);
@@ -41,7 +45,7 @@ const Properties = () => {
 
   return (
     <div className="mt-16">
-      <div className='flex flex-wrap justify-center w-100'>
+      <div className='flex flex-wrap justify-center'>
         {properties && properties.length > 0 ? (
           properties.map(property => (
             <div key={property.id} className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
@@ -61,8 +65,7 @@ const Properties = () => {
                   <p className="font-semibold text-lg"> {property.type}</p>
                   <p className="text-gray-400 ">Agency 1</p>
                   <p className="mt-8 text-sm text-gray-400">
-                    This will be the small description for the news you have shown
-                    here. There could be some great info.
+                    {property.description}
                   </p>
                   <div className="mt-3">
                     <Button
@@ -77,7 +80,23 @@ const Properties = () => {
             </div>
           )) 
         ) : (
-            <p>No properties available.</p> 
+            <div className='flex items-center justify-center h-screen w-full -mt-16'>
+              <div className="text-center">
+                <p class="mb-4 text-xl dark:text-gray-200">No properties available.</p>
+                <button
+                  className=' w-full'
+                  style={{
+                    color: 'white',
+                    backgroundColor: currentColor,
+                    borderRadius: '10px',
+                    padding: '15px',
+                  }}
+                  onClick={handleClick}
+                >
+                  Add Property
+                </button>
+              </div>
+            </div>
         )}
       </div>
     </div>
