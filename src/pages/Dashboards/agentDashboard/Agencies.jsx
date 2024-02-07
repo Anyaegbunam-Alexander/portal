43f8da570c0d12f44035789ccd4db05771eb490f
LanreSam <ezechelanre@gmail.com>
2024-02-07 05:53:07 +0100

    updated agencies

--- src/pages/Dashboards/agentDashboard/Agencies.jsx
+++ src/pages/Dashboards/agentDashboard/Agencies.jsx
@@ -8,22 +8,44 @@ import { GridComponent,
   Inject 
 } from '@syncfusion/ej2-react-grids';
 
-import { employeesData, employeesGrid } from '../../../data/dummy';
-import { Header } from '../../../components/agentDashboardComponent';
+import { Header } from '../../../components/dashboardComponents';
+import UsePropertyLogic from '../methods';
+import house from '../../../assets/1.png';
 
 const Agencies = () => {
+  
+  const {
+    getAllAgencies,
+    agenciesGrid,
+  } = UsePropertyLogic('https://realestate.api.sites.name.ng/agencies/');
+
+  const agenciesData = getAllAgencies; 
+
+  // Map through agenciesData and transform each agency as needed
+  const mappedData = agenciesData.map((agency) => {
+    return {
+      Profile: 'https://google.com',
+      Name: agency.name,
+      Email: agency.email,
+      Address: agency.address.street_address,
+      Country: agency.address.country,
+      State: agency.address.state,
+      EmployeeImage: house,
+    };
+  });
+ 
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl mt-16">
       <Header category="Page" title="Agencies"/>
       <GridComponent
-        dataSource={employeesData}
+        dataSource={mappedData}
         allowPaging
         allowSorting
         toolbar={['Search']}
         width="auto"
       >
         <ColumnsDirective>
-          {employeesGrid.map((item, index) => (
+          {agenciesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
