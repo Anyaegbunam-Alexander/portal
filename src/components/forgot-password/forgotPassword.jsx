dfe9def2c88473da4d67be30e5f0a9725a45bcaa
LanreSam <ezechelanre@gmail.com>
2024-04-02 12:39:25 +0100

    Working on the application size and forgot-password

--- src/components/forgot-password/forgotPassword.jsx
+++ src/components/forgot-password/forgotPassword.jsx
@@ -15,7 +15,7 @@ const ForgotPassword = () => {
 
     try {
       // Send a request to your backend API to handle the password reset
-      const response = await fetch('/api/forgot-password', {
+      const response = await fetch('https://realestate.api.mvcsites.name.ng/auth/request-password-reset-email/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -24,6 +24,8 @@ const ForgotPassword = () => {
       });
 
       const data = await response.json();
+
+      console.log(data);
 
       if (response.ok) {
         setMessage(data.message);
@@ -69,7 +71,7 @@ const ForgotPassword = () => {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-700 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
               disabled={isLoading}
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
